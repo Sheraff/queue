cfe19e8db917c58a3cc8da83453d2278ab77b546
Sheraff <fpellet@ensc.fr>
2024-06-11 18:30:51 +0200

    nothing

--- src/main.ts
+++ src/main.ts
@@ -14,17 +14,15 @@ registerPrograms({
 // ids should be static for idempotency, but for now we'll just generate random ids
 registerTask(crypto.randomUUID(), 'aaa', {})
 registerTask(crypto.randomUUID(), 'aaa', {})
-registerTask(crypto.randomUUID(), 'aaa', {})
 registerTask(crypto.randomUUID(), 'pokemon', { id: 2 })
 registerTask(crypto.randomUUID(), 'pokemon', { id: 151 })
 registerTask(crypto.randomUUID(), 'notask', { mimi: 'momo' })
+registerTask(crypto.randomUUID(), 'notask', { mimi: 'mumu' })
 
 do {
 	const hasMore = await handleNext()
 	if (hasMore === 'done') break
-	else if (hasMore === 'next') continue
-	else if (hasMore === 'wait') await new Promise(resolve => setTimeout(resolve, 10))
+	if (hasMore === 'next') continue
+	if (hasMore === 'wait') await new Promise(setImmediate)
 	else throw new Error('Unknown handleNext result')
 } while (true)
-
-

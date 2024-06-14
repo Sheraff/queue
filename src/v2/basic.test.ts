d3daf5cce680551ed25f0df775b99124fc7128f5
Sheraff <fpellet@ensc.fr>
2024-06-14 22:56:29 +0200

    improve basic tests

--- src/v2/basic.test.ts
+++ src/v2/basic.test.ts
@@ -1,13 +1,13 @@
 import assert from "node:assert"
 import { test } from 'node:test'
 import { Queue, createProgram, step } from "./queue"
-import { exhaustQueue } from "./test.utils"
+import { z } from "zod"
 
 test.describe('benchmark', () => {
 	test('synchronous', async (t) => {
 		const queue = new Queue({
 			hello: createProgram({ id: 'hello' }, async () => {
-				for (let i = 0; i < 100; i++) {
+				for (let i = 0; i < 10000; i++) {
 					await step.run('a', () => 'a')
 				}
 			})
@@ -16,8 +16,8 @@ test.describe('benchmark', () => {
 		await queue.registry.hello.invoke()
 		performance.mark('end')
 		const duration = performance.measure('hello', 'start', 'end').duration
-		t.diagnostic(`100 sync steps took ${duration.toFixed(2)}ms`)
-		assert(duration < 5, `Benchmark took ${duration}ms, expected less than 5ms`)
+		t.diagnostic(`10000 sync steps took ${duration.toFixed(2)}ms`)
+		assert(duration < 100, `Benchmark took ${duration}ms, expected less than 100ms`)
 		await queue.close()
 	})
 	test('asynchronous', async (t) => {
@@ -41,15 +41,21 @@ test.describe('benchmark', () => {
 test('memo', async (t) => {
 	let count = 0
 	const queue = new Queue({
-		hey: createProgram({ id: 'hey' }, async () => {
-			await step.run('ya', () => { count++ })
+		hey: createProgram({
+			id: 'hey',
+			input: z.object({ id: z.string() }),
+			output: z.object({ id: z.string() }),
+		}, async (input) => {
+			return step.run('ya', () => { count++; return input })
 		})
 	})
-	queue.registry.hey.dispatch()
-	queue.registry.hey.dispatch()
-	queue.registry.hey.dispatch()
-	queue.registry.hey.dispatch()
-	await exhaustQueue(queue)
-	assert.strictEqual(count, 1, 'Step should have been memoized')
+	queue.registry.hey.dispatch({ id: 'a' })
+	queue.registry.hey.dispatch({ id: 'a' })
+	queue.registry.hey.dispatch({ id: 'a' })
+	queue.registry.hey.dispatch({ id: 'b' })
+	queue.registry.hey.dispatch({ id: 'b' })
+	const res = await queue.registry.hey.invoke({ id: 'b' })
+	assert.strictEqual(count, 2, 'Step should have been memoized')
+	assert.deepEqual(res, { id: 'b' }, 'Step should have returned the correct value even when not re-invoked')
 	await queue.close()
 })
